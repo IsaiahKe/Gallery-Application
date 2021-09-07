41af840f8d0857018514673679b5882f3918eee5
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-09-07 05:40:16 +0300

    update admin dashboard

--- photos/admin.py
+++ photos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Pic,Location,Category
 
-# Register your models here.
+# Register your models here. 
+
+admin.site.register(Pic)
+admin.site.register(Location)
+admin.site.register(Category)
