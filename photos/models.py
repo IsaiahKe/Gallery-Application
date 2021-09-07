46702360c63625670c6a9b05874f9fb60fd09fc6
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-09-07 05:35:24 +0300

    create pic methods

--- photos/models.py
+++ photos/models.py
@@ -16,7 +16,20 @@ class Pic(models.Model):
     def save_image(self):
         self.save()
         
-   
+    @classmethod
+    def delete(cls,self,id):
+        query=Pic.objects.get(cls.id==id)
+        for item in query:
+            self.delete(item)
+            
+    def get_all():
+        query=Pic.objects.all()
+        return query
+    @classmethod
+    def search_category(cls,category):
+        query= cls.objects.filter(category=category)
+        return query
+    
 class Location(models.Model):
     name=models.CharField(max_length=30)
     def __str__(self):
